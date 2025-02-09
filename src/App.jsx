627755bcdcc2a9b587213752d8f1acbefcc01c43
agoooeng <145472348+agungciss@users.noreply.github.com>
2025-02-09 13:49:17 +0700

    tambah analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react"
+import { Analytics } from "@vercel/analytics/react"
 
 import MainContent from "./components/MainContent"
 
@@ -6,8 +8,8 @@ const App = () => {
   return (
     <>
       <div className="flex animate-fadeIn duration-1000">
-       
         <MainContent />
+        <Analytics />
       </div>
     </>
   )
